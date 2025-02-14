a19311cd82efa3f7424e53b35609480fb136f2eb
Relrige <91017454+Relrige@users.noreply.github.com>
2025-02-14 20:18:41 +0200

    Add project files.

---
+++ ConsoleApp1/Dog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Dog
+    {
+        int age;
+        public int Age 
+        { 
+            get { return age;}
+            set { age = value; }
+        }
+
+
+
+        public Dog() { }
+        public Dog(int age)
+        {
+            this.age = age;
+        }
+    }
+}
